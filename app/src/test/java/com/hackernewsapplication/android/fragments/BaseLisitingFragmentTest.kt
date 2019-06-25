857bb39f5d97b14c09865277f3c48edc3adfe387
Rahul Ravindran <rahul.r945@gmail.com>
2019-06-25 10:12:56 +0530

    - initial commit test coverage - changes in gradle for fragment_testing dep

--- app/src/test/java/com/hackernewsapplication/android/fragments/BaseLisitingFragmentTest.kt
+++ app/src/test/java/com/hackernewsapplication/android/fragments/BaseLisitingFragmentTest.kt
@@ -1,23 +1,33 @@
 package com.hackernewsapplication.android.fragments
 
+import androidx.fragment.app.FragmentFactory
+import androidx.fragment.app.testing.launchFragment
+import androidx.lifecycle.Lifecycle
+import androidx.test.runner.AndroidJUnit4
 import com.hackernewsapplication.android.entity.NewsEntity
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
-import org.junit.internal.runners.JUnit4ClassRunner
 import org.junit.runner.RunWith
 
-@RunWith(JUnit4ClassRunner::class)
+@RunWith(AndroidJUnit4::class)
 class BaseLisitingFragmentTest {
     lateinit var fragment: BaseListingFragment<NewsEntity>
-
+    lateinit var fragmentFactory: FragmentFactory
     @Before
     fun setUp() {
         fragment = BaseListingFragment()
+        fragmentFactory = FragmentFactory()
     }
 
     @Test
-    fun `lisiting fragment test`() {
-
+    fun `fragment initialisation tests`() {
+        val listingFragmentScenario = launchFragment<BaseListingFragment<NewsEntity>>(factory = fragmentFactory)
+        listingFragmentScenario.moveToState(Lifecycle.State.RESUMED)
+        listingFragmentScenario.onFragment {
+            Assert.assertNotNull(it.getAdapterType())
+            Assert.assertNotNull(it.adapter())
+        }
     }
 
 }
